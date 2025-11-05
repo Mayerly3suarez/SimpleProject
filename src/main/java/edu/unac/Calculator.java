1523ac86df4fae704cc53db27a29130c530c31bf
Mayerly Andrea Suarez <125830247+Mayerly3suarez@users.noreply.github.com>
2025-11-04 19:44:41 -0500

    clase calculator

--- src/main/java/edu/unac/Calculator.java
+++ src/main/java/edu/unac/Calculator.java
@@ -5,7 +5,15 @@ public class Calculator {
         return a + b;
     }
 
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
     public int multiply(int a, int b) {
         return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
     }
 }
